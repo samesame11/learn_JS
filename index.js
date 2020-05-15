f0d88a50ad99c03fcf61d65d3380e24d0fd70d8e
samesame11 <d-m-play@windowslive.com>
2020-05-15 15:28:53 +0000

    calculate price

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-var userNum = prompt("Please Fill the number that you want to play");
-let numberRandom = Math.random().toFixed(2)*100  
-if(userNum === numberRandom){
-  console.log("you win!")
-}else{
-  console.log("sorry you lose please try again the number of lotery is "+ numberRandom +" but your number is " + userNum)
-}
-
+let numOrder = prompt
